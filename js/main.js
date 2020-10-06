c54b6f07c25821c6851a96425540f2bd5d619d00
inbaeyoonkr <iby2455@gmail.com>
2020-10-06 22:15:21 +0900

    Set scroll height responsively

---
+++ js/main.js
@@ -0,0 +1,57 @@
+(() => {
+  let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+
+  const sceneInfo = [
+    {
+      type: "sticky",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-0"),
+      },
+    },
+    {
+      type: "normal",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-1"),
+      },
+    },
+    {
+      type: "sticky",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-2"),
+      },
+    },
+    {
+      type: "sticky",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-3"),
+      },
+    },
+  ];
+
+  function setLayout() {
+    sceneInfo.map((scene) => {
+      scene.scrollHeight = scene.heightNum * window.innerHeight;
+      scene.objs.container.style.height = `${scene.scrollHeight}px`;
+    });
+  }
+
+  function scrollLoop() {}
+
+  window.addEventListener("resize", setLayout);
+  window.addEventListener("scroll", () => {
+    yOffset = window.pageYOffset;
+    scrollLoop();
+  });
+
+  setLayout();
+
+  console.log(sceneInfo);
+})();
